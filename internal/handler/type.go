de4512c0d346de8d28247c9af85eb11b0c07d841
Andrey Kataev <kata3v.andrey@yandex.ru>
2026-04-23 15:54:32 +0300

    Коды для ошибок

--- internal/handler/type.go
+++ internal/handler/type.go
@@ -2,15 +2,16 @@ package handler
 
 import (
 	"context"
-	"errors"
 
 	pb "github.com/light-messenger/user-service/pkg/userservice"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var (
-	errEmptyNickname = errors.New("empty nickname")
-	errEmptyPassword = errors.New("empty password")
-	errIncorrectId   = errors.New("incorrect id")
+	errEmptyNickname = status.Error(codes.InvalidArgument, "empty nickname")
+	errEmptyPassword = status.Error(codes.InvalidArgument, "empty password")
+	errIncorrectId   = status.Error(codes.InvalidArgument, "incorrect id")
 )
 
 type Handler struct {
